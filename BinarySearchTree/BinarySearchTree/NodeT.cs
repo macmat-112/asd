9dabcb7607fac571f63668242b39a824b402d4c0
macmat-112 <147150618+macmat-112@users.noreply.github.com>
2024-11-04 00:45:06 +0100

    bst
    cpd

---
+++ BinarySearchTree/BinarySearchTree/NodeT.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BinarySearchTree
+{
+    internal class NodeT
+    {
+        public int data;
+        public NodeT root;
+        public NodeT left;
+        public NodeT right;
+
+        public static NodeT Insert(NodeT node, int value)
+        {
+
+            if (node == null)
+            {
+
+                node = new NodeT();
+                node.data = value;
+                return node;
+
+            }
+
+            if (value == node.data) return node;
+
+            if (value < node.data)
+            {
+
+                node.left = Insert(node.left, value);
+                node.left.root = node;
+
+            }
+            else if (value > node.data)
+            {
+
+                node.right = Insert(node.right, value);
+                node.right.root = node;
+
+            }
+
+            return node;
+
+        }
+
+        public static NodeT Delete(NodeT node, int value)
+        {
+
+            if (node == null) return node;
+
+            if (node.data > value) node.left = Delete(node.left, value);
+            else if (node.data < value) node.right = Delete(node.right, value);
+            else
+            {
+
+                if (node.left == null)
+                {
+
+                    if (node.right != null) node.right.root = node.root;
+                    node.root = null;
+                    return node.right;
+
+                }
+
+                if (node.right == null)
+                {
+
+                    node.left.root = node.root;
+                    node.root = null;
+                    return node.left;
+
+                }
+
+                NodeT succ = node.right;
+                while (succ != null && succ.left != null) succ = succ.left;
+                node.data = succ.data;
+                node.right = Delete(node.right, succ.data);
+
+            }
+
+            return node;
+
+        }
+
+        public static void CPDIn(NodeT node)
+        {
+
+            if (node == null)
+            {
+                return;
+            }
+
+            NodeT.CPDIn(node.left);
+            Console.Write(node.data.ToString() + " ");
+            NodeT.CPDIn(node.right);
+
+        }
+
+        public static void CPDPre(NodeT node)
+        {
+
+            if (node == null)
+            {
+                return;
+            }
+
+            Console.Write(node.data.ToString() + " ");
+            NodeT.CPDPre(node.left);
+            NodeT.CPDPre(node.right);
+
+        }
+
+        public static void CPDPost(NodeT node)
+        {
+
+            if (node == null)
+            {
+                return;
+            }
+
+            NodeT.CPDPost(node.left);
+            NodeT.CPDPost(node.right);
+            Console.Write(node.data.ToString() + " ");
+
+        }
+    }
+}
---
+++ BinarySearchTree/BinarySearchTree/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("BinarySearchTree")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BinarySearchTree")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("a6d006c8-dd58-4054-91ae-8d926b42c69d")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
